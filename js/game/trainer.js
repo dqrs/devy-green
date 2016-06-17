fae2da993700f039a4370e6cf86e671bd59cc909
David Gershuni <vidgersh@gmail.com>
2016-06-17 12:00:55 -0700

    removed trainer.js methods

--- js/game/trainer.js
+++ js/game/trainer.js
@@ -1,72 +1,6 @@
 class Trainer {
   
   /*
-    // Static Attributes:
-    firstName
-    lastName
-    age
-    slogan
-    favoriteColor
-    favoriteELement
-
-    // Dynamic Attributes (State/Vitals):
-    money
-    happiness
-    intelligence
-    energy
-    confidence
-
-    // string interpolation
-    getFullName()
-    getReverseName()
-    getDoubleFullName()
-    getDoubleReverseName()
-    getFirstNameLastInitial()
-    getFirstInitialLastName()
-    getImageFileName()
-    
-    // conditionals - threshold functions
-    getConfidenceDescription()
-    getHappinessDescription()
-    getIntelligenceDescription()
-    getLeague()
-    
-    // conditionals - other
-    getWeakestElement()
-
-    // calculations
-    getAgeInMonths()
-    getAgeInDays()
-    getAgeInWeeks()
-    getAgeInMinutes()
-    getAgeInSeconds()
-
-    // void functions
-    printHi()
-    printSlogan()
-    print(message)
-    sayHi()
-    saySlogan()
-    say(message)
-    
-    // Methods that update a single instance variable
-    setCoins(num)
-    lose10Coins()
-    gain10Coins()
-    loseCoins(num)
-    gainCoins(num)
-
-
-    // Methods that update multiple instance variables
-    work()
-    rest()
-    exercise()
-    watchTV()
-    readBook()
-    takeCompliment()
-    takeInsult()
-
-    giveCompliment()
     
   */
   constructor(options) {
@@ -83,67 +17,43 @@ class Trainer {
     this.strength = options.strength
   }
 
-  /*
-    Returns the name of the Pokemon's image file.
-    The image changes based on the Pokemon's species and
-    whether it is alive or not.
-    For example:
-      a Charmander pokemon's image file should be:
-        'Charmander.png' if it is alive
-        and 'Charmander-Dead.png' if it is dead.
-      a Blastoise pokemon's image file should be:
-        'Blastoise.png' if it is alive
-        and 'Blastoise-Dead.png' if it is dead.
-  */
-  // getImageFileName() {
-  //   return `${this.firstName} ${this.lastName}.png`
-  // }
-
-  // getLeague() {
-  //   // return "Implement this"
-  // }
-
   getFullName() {
-    // return this.firstName + this.lastName
-    return new Exception()
 
   }
 
   getReverseName() {
-    return `${this.lastName}, ${this.firstName}`
+    
   }
 
   getDoubleFullName() {
-    return `${this.firstName} ${this.firstName} ${this.lastName} ${this.lastName}`
+    // return `${this.firstName} ${this.firstName} ${this.lastName} ${this.lastName}`
   }
 
-
-  // getWeakestElement() {
-
-  // }
+  getImageFileName() {
+    // return `${this.firstName} ${this.lastName}.png`
+  }
 
   getFirstNameLastInitial() {
-    return `${this.firstName} ${this.lastName[0]}.`
   }
 
   getFirstInitialLastName() {
-    return `${this.firstName[0]}. ${this.lastName}`
+    // return `${this.firstName[0]}. ${this.lastName}`
   }
 
   writeHi() {
-    chatBubble("Hi")
+    // chatBubble("Hi")
   }
 
   writeSlogan() {
-    chatBubble(this.slogan)
+    // chatBubble(this.slogan)
   }
 
   sayHi() {
-    tts("Hi!")
+    // tts("Hi!")
   }
 
   saySlogan() {
-    tts(this.slogan)
+    // tts(this.slogan)
   }
 
   // say(message) {
